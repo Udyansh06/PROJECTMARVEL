4434f545f40789f9b4208d614ac17458e2a796ff
Udyansh06 <your-github-email@example.com>
2025-10-25 00:26:39 +0530

    Add background image

--- index.js
+++ index.js
@@ -53,6 +53,9 @@ app.get('/test-statici4', (req, res) => {
 app.get('/test-statici5', (req, res) => {
   res.sendFile(path.join(process.cwd(), 'public','image', 'generated-image2.png'));
 })
+app.get('/test-statici6', (req, res) => {
+  res.sendFile(path.join(process.cwd(), 'public','image', 'download__9_-removebg-preview.png'));
+})
 
 
 // import express from 'express';
