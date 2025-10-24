4cbf2ed533cac55cefd88e8d3b763f1ebd8f6f85
Udyansh06 <your-github-email@example.com>
2025-10-24 23:41:01 +0530

    f1

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get('/character/:id', (req, res) => {
   res.render('character.ejs', { character });
 });
 app.get('/test-static', (req, res) => {
-  res.sendFile(path.join(process.cwd(), 'public', 'index.css'));
+  res.sendFile(path.join(process.cwd(), 'public', 'mt.mp4'));
 });
 
 
