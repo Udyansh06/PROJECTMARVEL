b2a19dc36ae7607bc2f71a3a4ecf072a69955248
Udyansh06 <your-github-email@example.com>
2025-10-24 20:59:06 +0530

    vercel fix finall fs

--- index.js
+++ index.js
@@ -4,34 +4,28 @@ import crypto from 'crypto';
 import fs from 'fs';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 const publicKey = 'd6403b7807c973e35d0b6cbc3da7c94f';
 const privateKey = '7ee67dfce2b1f022e072f414c4bd29b0bfa5caf4';
 const baseURL = 'https://gateway.marvel.com/v1/public/characters';
+
 app.use(express.static("public"));
 
 function getMarvelHash(ts) {
-return crypto.createHash('md5').update(ts + privateKey + publicKey).digest('hex');
+  return crypto.createHash('md5').update(ts + privateKey + publicKey).digest('hex');
 }
 
+// If you want to fetch live data from Marvel API, uncomment and adjust the code below
 // app.get('/', async (req, res) => {
-// try {
+//   try {
 //     const heroes = ['Iron Man','Spider-Man (Peter Parker)','Thor', 'Captain america', 'Hulk','Loki','Hawkeye','Black Widow','Daredevil','Black Panther','Wolverine','Punisher','Doctor Strange','Deadpool','Moon Knight','Star Lord','Nova','Ghost Rider','Blade','Invisible Woman','Silver Surfer','Quicksilver','Thanos','Gambit','Winter Soldier'];
 //     const ts = Date.now().toString();
 //     const hash = getMarvelHash(ts);
-
 //     const heroPromises = heroes.map(async name => {
-//     const response = await axios.get(baseURL, {
-//         params: {
-//             name,
-//             ts,
-//             apikey: publicKey,
-//             hash
-//         }
-//         });
-//       return response.data.data.results[0]; // return first matching character info
-//     });
-
+//        const response = await axios.get(baseURL, {
+//          params: { name, ts, apikey: publicKey, hash }
+//        });
+//        return response.data.data.results[0];
+//      });
 //     const heroesData = await Promise.all(heroPromises);
 //     res.render('index.ejs', { data: heroesData });
 //     fs.writeFileSync('data.json', JSON.stringify(heroesData));
@@ -41,21 +35,20 @@ return crypto.createHash('md5').update(ts + privateKey + publicKey).digest('hex'
 //   }
 // });
 
-app.get('/',(req,res)=>{
-  res.render('index.ejs',{data:JSON.parse(fs.readFileSync('data.json'))});
-})
+app.get('/', (req, res) => {
+  res.render('index.ejs', { data: JSON.parse(fs.readFileSync('data.json')) });
+});
 
 app.get('/character/:id', (req, res) => {
   const reqid = parseInt(req.params.id);
   const allcharacters = JSON.parse(fs.readFileSync('data.json'));
-  const character = allcharacters.find((character) => character && character.id === reqid);
+  const character = allcharacters.find(
+    (character) => character && character.id === reqid
+  );
   if (!character) {
-    // handle not found case, like send error response or default value
     return res.status(404).send("Item not found");
   }
   res.render('character.ejs', { character });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+export default app;
